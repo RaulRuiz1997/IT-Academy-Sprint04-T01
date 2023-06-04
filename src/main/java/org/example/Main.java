8cd94c32be3597d853e3ec24290f6b7133fe412f
Raúl Ruiz <raul.ruizpedrosa@Outlook.com>
2023-06-04 04:03:18 +0200

    Ejercicio Singleto terminado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,66 @@ package org.example;
 
 import org.example.exercici1.Undo;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Undo undo = Undo.getUndo();
-        Undo undo2 = Undo.getUndo();
-        System.out.println(undo.hashCode());
-        System.out.println(undo2.hashCode());
+
+        String opcio = "0";
+        Scanner lector = new Scanner(System.in);
+
+        while (!opcio.equals("4")) {
+
+            System.out.println("Introdueix una opció:");
+            System.out.println("1. Afegir comanda");
+            System.out.println("2. Eliminar comanda");
+            System.out.println("3. Llistar últimes comandes");
+            System.out.println("4. Sortir");
+
+            opcio = lector.nextLine();
+
+            switch (opcio) {
+
+                case "1":
+                    System.out.println("Introdueix una comanda: ");
+                    undo.afegirComanda(lector.nextLine());
+                    break;
+
+                case "2":
+
+                    System.out.println(undo.llistarUltimesComandes());
+
+                    if (undo.getTamanyArrayComandes() != 0) {
+
+                        System.out.println("Selecciona la posició de la comanda a eliminar (La primera es el 0, la segona el 1 y la tercera el 2)");
+
+                        if (undo.eliminarComanda(Integer.parseInt(lector.nextLine()))) {
+                            System.out.println("Comanda eliminada\n");
+                        } else {
+                            System.out.println("Comanda no eliminada\n");
+                        }
+
+                    }
+
+                    break;
+
+                case "3":
+                    System.out.println(undo.llistarUltimesComandes());
+                    break;
+
+                case "4":
+                    System.out.println("Sortint de la aplicació");
+                    break;
+
+                default:
+                    System.out.println("¡Introdueix una opció correcta!\n");
+
+            }
+
+        }
 
     }
 
--- src/main/java/org/example/exercici1/Undo.java
+++ src/main/java/org/example/exercici1/Undo.java
@@ -1,11 +1,15 @@
 package org.example.exercici1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Undo {
 
     private static Undo instancia = null;
+    private List<String> comandes;
 
     private Undo() {
-
+        comandes = new ArrayList<>();
     }
 
     public synchronized static Undo getUndo() {
@@ -16,6 +20,31 @@ public class Undo {
 
         return instancia;
 
+    }
+
+    public void afegirComanda(String comanda) {
+        if (comandes.size() < 3) {
+            comandes.add(comanda);
+        }
+    }
+
+    public boolean eliminarComanda(int posicioComandaAEliminar) {
+        if (posicioComandaAEliminar >= 0 || posicioComandaAEliminar <= 2) {
+            if (comandes.size() > posicioComandaAEliminar) {
+                comandes.remove(posicioComandaAEliminar);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public String llistarUltimesComandes() {
+        return comandes.isEmpty() ? "Encara no hi ha comandes guardades!\n" : comandes + "\n";
+    }
+
+    public int getTamanyArrayComandes() {
+        return comandes.size();
     }
 
 }
